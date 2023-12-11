8c482af85c739f97e2c421b3a1a5a1e1dc881d69
Brian Harrington <brian.harrington@lyrasis.org>
2023-12-11 11:25:24 -0500

    Add resources by agent report

---
+++ backend/model/resources_by_agent_report.rb
@@ -0,0 +1,65 @@
+class ResourcesByAgentReport < AbstractReport
+  register_report(
+    params: [['scope_by_date', 'Boolean', 'Scope records by a date range'],
+             ['from', Date, 'The start of report range'],
+             ['to', Date, 'The start of report range']]
+  )
+
+  def initialize(params, job, db)
+    super
+
+    @date_scope = params['scope_by_date']
+
+    if @date_scope
+      from = params['from']
+      to = params['to']
+
+      raise 'Date range not specified.' if from === '' || to === ''
+
+      @from = DateTime.parse(from).to_time.strftime('%Y-%m-%d %H:%M:%S')
+      @to = DateTime.parse(to).to_time.strftime('%Y-%m-%d %H:%M:%S')
+
+      info[:scoped_by_date_range] = "#{@from} & #{@to}"
+    end
+  end
+
+  def query_string
+    date_condition = if @date_scope
+                      "resource.create_time > 
+                      #{db.literal(@from.split(' ')[0].gsub('-', ''))} 
+                      and resource.create_time < 
+                      #{db.literal(@to.split(' ')[0].gsub('-', ''))}"
+                    else
+                      '1=1'
+                    end
+    "SELECT resource.id as resource_system_id, 
+      resource.identifier as resource_number, resource.title as Title, resource.create_time, extent_number, extent_type, ifnull(ifnull(ifnull(name_person.sort_name, name_family.sort_name), name_corporate_entity.sort_name), 'Unknown') as name, role_id as `function`, relator_id as role
+      FROM resource
+      LEFT JOIN linked_agents_rlshp ON (resource.id = linked_agents_rlshp.resource_id)
+      left outer join name_person
+        on name_person.agent_person_id = linked_agents_rlshp.agent_person_id
+      left outer join name_family
+        on name_family.agent_family_id = linked_agents_rlshp.agent_family_id
+      left outer join name_corporate_entity
+        on name_corporate_entity.agent_corporate_entity_id = 
+        linked_agents_rlshp.agent_corporate_entity_id
+      natural left outer join
+        (select
+          resource_id,
+          sum(number) as extent_number,
+          GROUP_CONCAT(distinct extent_type_id SEPARATOR ', ') as extent_type
+        from extent
+        group by resource_id) as extent_cnt
+      WHERE repo_id = #{db.literal(@repo_id)} AND #{date_condition}
+        AND linked_agents_rlshp.resource_id IS NOT NULL
+      ORDER BY resource.create_time DESC"
+  end
+
+  def fix_row(row)
+    ReportUtils.fix_identifier_format(row, :resource_number)
+    ReportUtils.get_enum_values(row, [:extent_type, :function, :role])
+    ReportUtils.fix_extent_format(row)
+    row.delete(:resource_system_id)
+  end
+
+end
